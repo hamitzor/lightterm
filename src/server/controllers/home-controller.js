0657623f61692265a1afa7fd25af34f521996de7
Hamit Zor <thenrerise@gmail.com>
2020-01-07 01:05:50 +0100

    corrected index.html path

--- src/server/controllers/home-controller.js
+++ src/server/controllers/home-controller.js
@@ -2,5 +2,5 @@ const path = require('path')
 
 /* Send index.html file to the client */
 module.exports = (req, res) => {
-   res.sendFile(path.resolve(__dirname, '../src/client/index.html'))
+   res.sendFile(path.resolve(__dirname, '../../../src/client/index.html'))
 }
